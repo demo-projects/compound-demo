1ae2fe02ca9319d7d12c4a150bb50909e4e487e3
nirkaufman <nirkaufman@gmail.com>
2018-09-05 23:33:44 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'milligram/dist/milligram.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
